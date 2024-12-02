3eaa874b2925a228f2c63c3dcefafe6d16b2d509
Rafael_Pinheiro <128435838+PinheiroXt660@users.noreply.github.com>
2024-12-02 03:49:57 -0300

    comit
    comit do dia 02/12/2024 a 03:50 da manhã onde tudo esta indo bem, depois te um tempo ficou pior e agora regaçou foi tudo.... boa noite a vcs

---
+++ login.js
@@ -0,0 +1,28 @@
+
+function validityFields() {
+    const emailValid = isEmailValid();
+    document.getElementById('recorver-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid(); 
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+}
+
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function isPasswordValid() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        return false
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+}
